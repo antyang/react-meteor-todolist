ade763fa0b23ef0168cb82930915a123e138522f
Antony Yang <mrant0ny@gmail.com>
2015-08-30 20:48:57 -0700

    insert into collections

--- App.jsx
+++ App.jsx
@@ -1,17 +1,17 @@
 App = React.createClass({
 
-	// this misxin makes the getMeteorData method work
+	// this mixins makes the getMeteorData method work
 	mixins: [ReactMeteorData],
 	
 	// loads items from the Tasks collection and puts them on this.data.tasks
 	getMeteorData(){
 		return {
-			tasks: Tasks.find({}).fetch()
+			tasks: Tasks.find({}, {sort: {createdAt: -1} }).fetch()
 		}
 	},
 
 
-	/** Hardedcode Dummydata
+	/* Hardedcode Dummydata
 	getTasks(){
 		return [
 		{_id: 1, text: 'This is task 1'},
@@ -27,12 +27,33 @@ App = React.createClass({
 		});
 	},
 
+	handleSubmit(event){
+		event.preventDefault();
+
+		// find the text field via the React ref
+		var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+		// inserting into collection
+		Tasks.insert({
+			text: text,
+			createdAt: new Date() // current time
+		});
+
+		// clear form
+		React.findDOMNode(this.refs.textInput).value = "";
+	},
+
 	render: function(){
 		return (
 				<div className="container">
 					<header>
 						<h1>Todo List</h1>
+						
+						<form onSubmit={ this.handleSubmit } className="new-task">
+							<input type="text" ref="textInput" placeholder="Type to add new tasks"/>
+						</form>
 					</header>
+
 
 					<ul>
 						{ this.renderTasks() }
